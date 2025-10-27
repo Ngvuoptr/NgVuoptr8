737c0b062f9978bc9a601358eacd31dbdcfc4aa3
VuDev <nguyenvu.optr@outlook.com.vn>
2025-10-27 11:08:08 +0700

    Create hello.py
    Tạo hell.py
    
    Signed-off-by: VuDev <nguyenvu.optr@outlook.com.vn>

---
+++ .github/.github/.github/workflows/examples/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Ví dụ nhỏ: hello.py
+Chạy: python examples/hello.py
+"""
+def main():
+    print("Xin chào — đây là ví dụ Hello World từ kho Ngvuoptr!")
+
+if __name__ == "__main__":
+    main()
